baf062c5d7c6a0b75aefb006f56560d67487c5cd
tubri-github <111301456+tubri-github@users.noreply.github.com>
2022-11-07 13:29:51 -0600

    fixed wrong labels

--- utils.py
+++ utils.py
@@ -3,26 +3,25 @@ import cv2
 
 
 def centroid_histogram(clt):
-    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
-
-    # normalize
+    number_labels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=number_labels)
     hist = hist.astype("float")
     hist /= hist.sum()
-
     return hist
 
 
 def plot_colors(hist, centroids):
 
-    bar = np.zeros((50, 300, 3), dtype="uint8")
+    bars = np.zeros((60, 300, 3), dtype="uint8")
     startX = 0
 
     for (percent, color) in zip(hist, centroids):
-        # plot the relative percentage of each cluster
         endX = startX + (percent * 300)
-        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-                      color.astype("uint8").tolist(), -1)
+        cv2.rectangle(bar, 
+                      (int(startX), 0),
+                      (int(endX), 50),
+                      color.astype("uint8").tolist(), 
+                      -1)
         startX = endX
 
-    return bar
+    return bars
